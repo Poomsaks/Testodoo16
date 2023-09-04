472e7e27b5102c4f7dd414c511d3bb46e0fbef0f
Poomsaks <138859913+Poomsaks@users.noreply.github.com>
2023-09-04 18:15:42 +0700

    Add files via upload

---
+++ __manifest__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Test Odoo16",
+    'author': "BNE",
+    'description': '',
+    'version': '0.1',
+    'depends': ['base'],
+    'data': [],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+    'images': ['static/description/icon.png'],
+}
